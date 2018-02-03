8eb9fe5a33f05021c1167b8762fb3bd92df8aa05
GWHS Robotics Team 5507 <gwhsrobotics129@gmail.com>
2018-02-03 12:48:24 -0800

    removed Configtalon from Drivetrain

--- src/org/usfirst/frc/team5507/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5507/robot/subsystems/DriveTrain.java
@@ -49,35 +49,6 @@ public class DriveTrain extends Subsystem {
 		Constants.configTalon(backLeft);
 	}
 	
-	 // config a talon motor controller with an Encoder
-	public static void configTalon(WPI_TalonSRX talon) {
-		talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
-		talon.setSensorPhase(true);
-		talon.setInverted(false);
-		
-		/* Set relevant frame periods to be at least as fast as periodic rate*/
-		talon.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, Constants.kTimeoutMs);
-		talon.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, Constants.kTimeoutMs);
-
-		/* set the peak and nominal outputs */
-		talon.configNominalOutputForward(0, Constants.kTimeoutMs);
-		talon.configNominalOutputReverse(0, Constants.kTimeoutMs);
-		talon.configPeakOutputForward(1, Constants.kTimeoutMs);
-		talon.configPeakOutputReverse(-1, Constants.kTimeoutMs);
-		
-		/* set closed loop gains in slot0 - see documentation */
-		talon.selectProfileSlot(Constants.kSlotIdx, Constants.kPIDLoopIdx);
-		talon.config_kF(0, 0.2, Constants.kTimeoutMs);
-		talon.config_kP(0, 0.2, Constants.kTimeoutMs);
-		talon.config_kI(0, 0, Constants.kTimeoutMs);
-		talon.config_kD(0, 0, Constants.kTimeoutMs);
-		/* set acceleration and vcruise velocity - see documentation */
-		talon.configMotionCruiseVelocity(15000, Constants.kTimeoutMs);
-		talon.configMotionAcceleration(6000, Constants.kTimeoutMs);
-		/* zero the sensor */
-		talon.setSelectedSensorPosition(0, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
-	}
-	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
