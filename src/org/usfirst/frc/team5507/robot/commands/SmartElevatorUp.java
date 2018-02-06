2eb20b440f6ab0d3093016447537c1d9e98f1098
GWHS Robotics Team 5507 <gwhsrobotics129@gmail.com>
2018-02-05 16:03:18 -0800

    Stuff

--- src/org/usfirst/frc/team5507/robot/commands/SmartElevatorUp.java
+++ src/org/usfirst/frc/team5507/robot/commands/SmartElevatorUp.java
@@ -26,7 +26,7 @@ public class SmartElevatorUp extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.m_smartElevator.getPositionForState(Robot.m_smartElevator.setNextStateUp());
+	//	Robot.m_smartElevator.goUp();
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
