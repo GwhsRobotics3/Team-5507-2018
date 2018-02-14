860c4512d9d2de93bbaeb6718418aacf6eb55d10
GwhsRobotics3 <gwhsrobotics129@gmail.com>
2018-02-14 15:41:35 -0800

    new autonomous command

---
+++ src/org/usfirst/frc/team5507/robot/commands/AutonomousWait.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team5507.robot.commands;
+
+import org.usfirst.frc.team5507.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutonomousWait extends Command {
+
+	private Timer autoTimer;
+	private double timeWaiting;
+	
+    public AutonomousWait(double t) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.m_driveTrain);
+    	timeWaiting = t;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	autoTimer.reset();
+    	autoTimer.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	return autoTimer.get() > timeWaiting;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
