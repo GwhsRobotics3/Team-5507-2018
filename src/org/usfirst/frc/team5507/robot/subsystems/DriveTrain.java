2d65a9bc40002965751bf335637045a0e25f2bdd
GWHS Robotics Team 5507 <gwhsrobotics129@gmail.com>
2018-02-17 15:47:27 -0800

    SmartDashboard Layout

--- src/org/usfirst/frc/team5507/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5507/robot/subsystems/DriveTrain.java
@@ -36,7 +36,7 @@ public class DriveTrain extends Subsystem {
 		ConfigTalon.configTalon(frontRight);
 		ConfigTalon.configTalon(backRight);
 		ConfigTalon.configTalon(backLeft);
-		addChild("front left talon",frontLeft);
+		addChild("front left talon", frontLeft);
 		addChild("front right talon", frontRight);
 		addChild("back left talon", backLeft);
 		addChild("back right talon", backRight);
